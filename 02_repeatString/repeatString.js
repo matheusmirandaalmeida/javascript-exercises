b470d737f26ca7083e2254f42130a8e60ee7d5bb
Matheus M Almeida <matheus.mma2015@gmail.com>
2023-08-08 02:50:50 -0300

    exercicio 04
    removendo do array

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(world, time) {
+    if(time < 0) return "ERROR";
+    let string = '';
+    for(let i = 0; i < time; i++){
+        string += world
+    }
+    return string;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    const newArray = [];
+    
+    array.forEach(element => {
+        if(!newArray.includes(element)){
+            newArray.push(element);
+        }
+    });
+    array =  newArray;
+    return array;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
