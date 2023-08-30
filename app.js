2a0c377c88ad388669e583ea2bfedbeccc255a30
Matheus M Almeida <matheus.mma2015@gmail.com>
2023-08-30 09:00:52 -0300

    Exercício de pratica de LDP

---
+++ app.js
@@ -0,0 +1,10 @@
+let myPocket = 10;
+let coxinha = 5;
+let pastel = 7;
+
+let myChange = myPocket - coxinha - pastel;
+if (myChange >= 0) {
+    console.log('Eu posso comprar os dois produtos');
+} else {
+    console.log('Não posso comprar os dois produtos juntos')
+}
